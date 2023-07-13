4462b0387aaaba705f0f780efffbae430a4b309a
Olha Soroka <olha.soroka@inventorsoft.co>
2023-07-13 11:02:06 +0300

    initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <WeatherWidgetComponent />
+
+
+    <button @click="weatherWidgetStore.changeLocation('London')">Change location</button>
+  </div>
+</template>
+
+<script setup>
+const {weatherWidgetStore} = useWeatherWidgetComposable();
+</script>
