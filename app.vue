c73cb3afccd2999fe403a4b699221d44f333f38a
Olha Soroka <olha.soroka@inventorsoft.co>
2023-07-13 20:18:12 +0300

    info_added

--- app.vue
+++ app.vue
@@ -50,10 +50,10 @@ const options = [
   },
 ];
 const cities = {
-Kiev: 'Kyiv, the capital city of Ukraine, is a vibrant metropolis located on the banks of the Dnieper River. It is a city with a rich history, stunning architecture, and a unique blend of Eastern European traditions and modernity. Kyiv offers a wide range of attractions and activities for tourists to enjoy.',
-London: 'London, the capital city of the United Kingdom, is a vibrant and cosmopolitan metropolis that offers a wealth of history, culture, and iconic landmarks. It is a city that seamlessly blends tradition with modernity, making it a top destination for tourists from around the world.',
-Berlin: 'Berlin, the capital city of Germany, is a vibrant and dynamic metropolis that effortlessly combines history, culture, and creativity. Known for its rich heritage, diverse neighborhoods, and vibrant arts scene, Berlin is a top destination for travelers from around the globe.',
-'New-York': 'New York City, often referred to as the "Big Apple," is a vibrant and diverse metropolis that captures the essence of urban life. It is one of the most iconic cities in the world, renowned for its skyline, cultural institutions, and energetic atmosphere.'
+Kiev: "Kyiv, the capital city of Ukraine, is a vibrant metropolis located on the banks of the Dnieper River. It is a city with a rich history, stunning architecture, and a unique blend of Eastern European traditions and modernity. Kyiv offers a wide range of attractions and activities for tourists to enjoy.Whether you're interested in history, culture, or simply enjoying the vibrant city atmosphere, Kyiv is a destination that will leave a lasting impression on any visitor.",
+London: 'London, the capital city of the United Kingdom, is a vibrant and cosmopolitan metropolis that offers a wealth of history, culture, and iconic landmarks. It is a city that seamlessly blends tradition with modernity, making it a top destination for tourists from around the world.London is known for its rich history, spanning over two thousand years. It is home to world-famous landmarks, such as the majestic Buckingham Palace, the iconic Big Ben, and the historic Tower of London.',
+Berlin: 'Berlin, the capital city of Germany, is a vibrant and dynamic metropolis that effortlessly combines history, culture, and creativity. Known for its rich heritage, diverse neighborhoods, and vibrant arts scene, Berlin is a top destination for travelers from around the globe.Berlin has a fascinating history that is reflected in its architecture, memorials, and museums. The city played a significant role in both World War II and the Cold War, and remnants of its past can be seen throughout its streets and landmarks. ',
+'New-York': 'New York City, often referred to as the "Big Apple," is a vibrant and diverse metropolis that captures the essence of urban life. It is one of the most iconic cities in the world, renowned for its skyline, cultural institutions, and energetic atmosphere.New York City is located in the northeastern United States and is composed of five boroughs: Manhattan, Brooklyn, Queens, The Bronx, and Staten Island. It is a global center of finance, media, art, fashion, and entertainment, making it a melting pot of cultures and influences.'
 }
 
 </script>
@@ -86,7 +86,7 @@ Berlin: 'Berlin, the capital city of Germany, is a vibrant and dynamic metropoli
 }
 
 .content_container {
-  padding: 50px 200px;
+  padding: 50px 250px;
   display: flex;
   justify-content: space-between;
   align-items: center;
